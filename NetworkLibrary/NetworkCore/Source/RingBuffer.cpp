09082bfb5b9fac3e1729fe31a31ec2b185367b7c
박주현 <yeochi1201@gmail.com>
2025-11-20 12:29:07 +0900

    feat: Get RingBuffer State Func

--- NetworkLibrary/NetworkCore/Source/RingBuffer.cpp
+++ NetworkLibrary/NetworkCore/Source/RingBuffer.cpp
@@ -5,6 +5,11 @@ RingBuffer::RingBuffer(size_t bufSize)
 {
 }
 
+RingBuffer::~RingBuffer()
+{
+    Close();
+}
+
 RingBuffer::RingBuffer(RingBuffer&& other) noexcept
     : mBuf(std::move(other.mBuf)), mBufSize(other.mBufSize), mReadPos(other.mReadPos), mWritePos(other.mWritePos), mIsFull(other.mIsFull)
 {
@@ -29,4 +34,52 @@ RingBuffer& RingBuffer::operator=(RingBuffer&& other) noexcept
         other.mIsFull   = false;
     }
     return *this;
+}
+
+void RingBuffer::Reset()
+{
+    mReadPos  = 0;
+    mWritePos = 0;
+    mIsFull   = false;
+}
+
+void RingBuffer::Close()
+{
+    mReadPos  = 0;
+    mWritePos = 0;
+    mIsFull   = false;
+    mBuf.reset();
+}
+
+size_t RingBuffer::BufSize() const noexcept
+{
+    return mBufSize;
+}
+
+size_t RingBuffer::WriteSpace() const noexcept
+{
+    if (mIsFull) {
+        return mBufSize;
+    }
+    if (mWritePos >= mReadPos) {
+        return mWritePos - mReadPos;
+    } else {
+        return mBufSize - (mReadPos - mWritePos);
+    }
+}
+size_t RingBuffer::FreeSpace() const noexcept
+{
+    if (mIsFull) {
+        return 0;
+    }
+    return mBufSize - WriteSpace();
+}
+bool RingBuffer::IsEmpty() const
+{
+    return !mIsFull && (mReadPos == mWritePos);
+}
+
+bool RingBuffer::IsFull() const
+{
+    return mIsFull;
 }
