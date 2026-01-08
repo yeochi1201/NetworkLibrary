91c298d9d405f49e057245c3b68e3c0311c5ddfc
박주현 <yeochi1201@gmail.com>
2026-01-08 12:50:07 +0900

    feat: Parser Pull & Pop Func

--- NetworkLibrary/NetworkCore/Source/HttpParser.cpp
+++ NetworkLibrary/NetworkCore/Source/HttpParser.cpp
@@ -18,4 +18,55 @@ std::string HttpParser::ToLower(std::string_view s){
     out.reserve(s.size());
 
     for(char c : s) out.push_back((char)std::tolower((unsigned char)c));
+
+    return out;
 }
+
+std::string HttpParser::Trim(std::string_view s){
+    std::size_t b = 0;
+    std::size_t e = s.size();
+
+    while (b < e && std::isspace((unsigned char)s[b])) b++;
+    while (e > b && std::isspace((unsigned char)s[e-1])) e--;
+
+    return std::string(s.substr(b, e-b));
+}
+
+bool HttpParser::SplitOnce(std::string_view s, char delim, std::string_view& left, std::string_view& right){
+    auto pos = s.find(delim);
+    if(pos == std::string_view::npos) return false;
+
+    left = s.substr(0, pos);
+    right = s.substr(pos+1);
+
+    return true;
+}
+
+bool HttpParser::PullFromRecvBuffer(RecvBuffer& rb, std::size_t maxPull){
+    std::size_t  available = rb.WriteSpace();
+    if(available == 0) return false;
+
+    if(available > maxPull) available = maxPull;
+
+    std::string tmp;
+    tmp.resize(available);
+
+    std::size_t peeked = 0;
+    if(rb.Peek(tmp.data(), available, peeked)!= RecvBuf_Ok || peeked == 0) return false;
+
+    mBuf.append(tmp.data(), peeked);
+    if(rb.Consume(peeked) != RecvBuf_Ok) return false;
+
+    return true;
+}
+
+bool HttpParser::PopLine(std::string& outLine){
+    auto pos = mBuf.find("\r\n");
+    if(pos == std::string::npos) return false;
+
+    outLine = mBuf.substr(0, pos);
+    mBuf.erase(0, pos+2);
+
+    return true;
+}
+
